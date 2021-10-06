9029b5b17d8305c8984407f00d81155dcd01bbb0
Errol Kaylor <errolk@uoregon.edu>
2021-10-06 17:07:27 -0500

    Added Markdown file, to test rmarkdown.
    Adjusted master_script file to read in data files.

--- scripts/master_script.R
+++ scripts/master_script.R
@@ -1,2 +1,17 @@
 library(dplyr)
-x <- c(1:10)
+library(here)
+library(rio)
+library(tidyverse)
+path <- here("data/Project_Reads_Scores.csv.csv")
+path
+read.csv(path)
+(here("data/Fatality.txt.txt"))
+list.files(here())
+Project_Reads_Scores <- import(path) %>%
+  as_tibble()
+Project_Reads_Scores
+x <- here("data/Fatality.txt.txt")
+truncheon <- data.frame(x,row.names = "Fun")
+rio::export(x = Project_Reads_Scores,format = "csv")
+rio::export(x= truncheon,format="dta")
+
